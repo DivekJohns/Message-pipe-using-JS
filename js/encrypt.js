5f019b0fd6f27c089e09e238657249a722d4e81a
Divek John <39296126+DivekJohns@users.noreply.github.com>
2018-07-31 11:09:54 +0530

    Update encrypt.js

--- js/encrypt.js
+++ js/encrypt.js
@@ -4,6 +4,7 @@ baseString=decodeURI(str[1]);
 key=decodeURI(str[2]);
 var encodedString = window.btoa( baseString );
 document.write("ecrypted string is "+encodedString+"<br>") 
+//just for testing please change the url address before working !
 var generatedUrl='file:///C:/Users/omegajohn/Desktop/dec.html'+'?q='+encodedString+'?q='+window.btoa(key);
 document.write("<a href="+generatedUrl+">click here to decode the string</a>");
 var decodedString = window.atob( encodedString ); 
